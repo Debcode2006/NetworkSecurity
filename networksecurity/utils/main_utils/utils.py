60a553643892ae9dba82d01fffe1b4f57f16cac5
Debcode2006 <sarkardebanjan137@gmail.com>
2025-12-07 03:15:36 +0530

    data validation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -12,3 +12,15 @@ def read_yaml_file(filepath:str)-> dict:
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
+def write_yaml_file(filepath:str, content:object, replace:bool=False)-> dict:
+    try:
+        if replace:
+            if os.path.exists(filepath):
+                os.remove(filepath)
+            os.makedirs(os.path.dirname(filepath),exist_ok=True)
+        with open(filepath, "w") as yaml_obj:
+            return yaml.safe_dump(content, yaml_obj)
+        
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
